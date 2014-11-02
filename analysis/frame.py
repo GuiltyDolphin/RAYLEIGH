07956e720aa974176f13378bc343560156ff0a30
Ben Moon <guiltydolphin@gmail.com>
2014-11-02 16:03:30 +0000

    Add features
    Pickle:
    - Can create pickle objects
    
    - Add regular expression parsing
      Can now parse:
        - Title
        - Datatype
        - Values
        - Headers
    
    Parsing:
    - Can parse config
    - Can parse DSC lists
    
    - Accepting alternate names
    
    - Creates correct frame (dsc) object

--- analysis/frame.py
+++ analysis/frame.py
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 # frame.py
 
+import pickle as pk
+import re
+from collections import deque
+
+pk_protocol = 4
 
 class Frame:
     """Frame object to represent the data contained within .dsc files"""
@@ -23,3 +28,94 @@ class Frame:
         self._acquisition_start_time = kwargs.get("acquisition_start_time", None)
         self._acquisition_start_time_string = kwargs.get("acquisition_start_time_string", None)
         self._timepix_clock = kwargs.get("timepix_clock", None)
+
+
+class DSCParser:
+    """Parser for DSC files"""
+    def __init__(self):
+        self._blank_frame = Frame()
+        self._dsc_reg = {
+                'title': re.compile('"?(?P<short_name>(?:\w\s?)+)[^"]*"?(?:\s+\("(?P<long_name>(?:\w\s?)+)(?:[\[(].+[)\]])?"\):)?'),
+                'data_type': re.compile("(?P<type>\w*)\[(?P<num>\w*)\]"),
+                'value': re.compile("(?P<value>.+)"),
+                'header': re.compile("Type=(?P<type>.+) width=(?P<width>\d+) height=(?P<height>\d+)")
+                }
+
+    def _pickle_from_dsc(self, dsc_data):
+        """Produce a pickled Frame object from a .dsc file"""
+        frame = self._frame_from_dsc(dsc_data)
+        return pk.dumps(frame, pk_protocol)
+
+    def _frame_from_dsc(self, dsc_data):
+        """Parse a .dsc file and create a frame object"""
+        config = deque([s.splitlines() for s in dsc_data.split("\n\n")])
+        config.remove([])
+
+        header = config.popleft()
+
+        frame, header_rest = self._analyse_dsc_header(header)
+        if header_rest:
+            config.appendleft(header_rest)
+        kvs = {'frame': frame}
+        use_short_names = ["DACs values of all chips",
+                           "Medipix or chipboard ID"]
+        expect_list = [("DACs", " ")]
+        use_alternate = [('[Ss]tart time \(string\)', "acquisition start time string"),
+                ('ChipboardID', 'chipboard id')]
+        for conf in config:
+            kvs.update(self._parse_config(conf, use_short_names, expect_list, use_alternate))
+
+        return Frame(**kvs)
+
+    def _analyse_dsc_header(self, header):
+        """Analyse a .dsc header. Return a dictionary for
+        header values and the header attachment if any"""
+        header_type = self._dsc_reg['header'].match(header[2]).groupdict()
+        header_type['name'] = self._dsc_reg['title'].match(header[0]).groupdict()['short_name']
+        for entry in ['height', 'width']:
+            header_type[entry] = int(header_type[entry])
+        return (header_type, header[3:])
+
+    def _parse_config(self, config, use_short=[], expect_list=[], use_alternate=[]):
+        """Retrieve name and value of a config section"""
+        title = self._dsc_reg['title'].match(config[0]).groupdict()
+        str_value = self._dsc_reg['value'].match(config[2]).groupdict()['value']
+
+        def try_str_to_num(s):
+            """Attemp to convert a string to an int or float"""
+            try:
+                value = int(s)
+            except ValueError:
+                try:
+                    value = float(s)
+                except ValueError:
+                    value = s
+            return value
+
+        exps = [x[0] for x in expect_list]
+        for name in expect_list:
+            if title['short_name'] == name[0] or title['long_name'] == name[0]:
+                value = str_value.split(name[1])
+                value = list(map(try_str_to_num, value))
+                if ('') in value:
+                    value.remove('')
+                break
+        else:
+            value = try_str_to_num(str_value)
+
+        def format_title(title):
+            return title.strip().replace(" ", "_").lower()
+
+        for a in use_alternate:
+            if re.findall(a[0], config[0]):
+                title['short_name'] = a[1]
+                title['long_name'] = a[1]
+                break
+
+        if title['long_name'] in use_short:
+            return {format_title(title['short_name']): value}
+        else:
+            return {format_title(title['long_name']): value}
+
+
+
--- tests/test_frame.py
+++ tests/test_frame.py
@@ -5,8 +5,39 @@
 import unittest
 import tempfile
 import os
+import json
+import copy
+import pickle as pk
+import re
 
 from analysis import frame
+
+data_frame = frame.Frame(
+    frame={
+        'name': "A000000001",
+        'type': "i16 [X,Y,C]",
+        'width': 256,
+        'height': 256},
+    acquisition_mode=1,
+    acquisition_time=60.0,
+    chipboard_id="B06-W0212",
+    dacs=[1, 100, 255, 127, 127,
+          0, 405, 7, 130, 128, 80,
+          85, 128, 128],
+    firmware_version="Firmware 3 (date: 28. 11. 2012)",
+    bias_voltage=95.0,
+    hw_timer_mode=2,
+    medipix_interface="MX-10",
+    medipix_clock=10.0,
+    medipix_type=3,
+    name_and_serial_number="MX-10 Particle Detector A",
+    pixelman_version="2.2.2",
+    detector_polarity=1,
+    acquisition_start_time=1396447375.004957,
+    acquisition_start_time_string="Wed Apr 02 15:02:55.004957 2014",
+    timepix_clock=10.0)
+
+pk_protocol = 4
 
 
 class TestFrame(unittest.TestCase):
@@ -14,37 +45,13 @@ class TestFrame(unittest.TestCase):
     """Tests for the Frame object"""
 
     def setUp(self):
-        self.frame = frame.Frame(
-            frame={
-                'name': "A000000001",
-                'unknown': ['F0'],
-                'type': "i16 [X, Y, C]",
-                'width': 256,
-                'height': 256},
-            acquisition_mode=1,
-            acquisition_time=60.0,
-            chipboard_id="B06-W0212",
-            dacs=[1, 100, 255, 127, 127,
-                  0, 405, 7, 130, 128, 80,
-                  85, 128, 128],
-            firmware_version="Firmware 3 (date: 28. 11. 2012)",
-            bias_voltage=95.0,
-            hw_timer_mode=2,
-            medipix_interface="MX-10",
-            medipix_clock=10.0,
-            medipix_type=3,
-            name_and_serial_number="MX-10 Particle Detector A",
-            pixelman_version="2.2.2",
-            detector_polarity=1,
-            acquisition_start_time=1396447375.004957,
-            acquisition_start_time_string="Wed Apr 02 15:02:55.004957 2014",
-            timepix_clock=10.0)
+        self.frame = copy.deepcopy(data_frame)
 
-        def tearDown(self):
-            pass
+    def tearDown(self):
+        pass
 
     def test_has_frame_class(self):
-        self.assertIsInstance(self.frame, frame.Frame)
+        self.assertIsInstance(data_frame, frame.Frame)
 
 
 class TestDSCParser(unittest.TestCase):
@@ -52,6 +59,9 @@ class TestDSCParser(unittest.TestCase):
     """Tests for the DSC Parser object"""
 
     def setUp(self):
+        self.frame = copy.deepcopy(data_frame)
+        self.pickled_frame = pk.dumps(self.frame, pk_protocol)
+        self.parser = frame.DSCParser()
         self.in_file = "testdata/frame_data.txt.dsc"
         self.out_file = tempfile.NamedTemporaryFile(delete=False)
         with open(self.in_file) as f:
@@ -60,5 +70,85 @@ class TestDSCParser(unittest.TestCase):
     def tearDown(self):
         os.remove(self.out_file.name)
 
-    def test_name(self):
-        pass
+    def test_can_match_title(self):
+        title_matcher = self.parser._dsc_reg['title']
+        title = '"Acq time" ("Acquisition time [s]"):'
+        matches = title_matcher.match(title).groupdict()
+        short = matches['short_name']
+        self.assertEqual('Acq time', short)
+        long_name = matches['long_name']
+        self.assertEqual('Acquisition time ', long_name)
+
+    def test_can_match_data_type(self):
+        type_matcher = self.parser._dsc_reg['data_type']
+        data_type = "double[1]"
+        matches = type_matcher.match(data_type).groupdict()
+        type_ = matches['type']
+        self.assertEqual(type_, 'double')
+        num = matches['num']
+        self.assertEqual(num, '1')
+
+    def test_can_match_value(self):
+        value_matcher = self.parser._dsc_reg['value']
+        value = "60.000000"
+        matches = value_matcher.match(value).groupdict()
+        val = matches['value']
+        self.assertEqual(val, '60.000000')
+
+    def test_can_match_header(self):
+        header_matcher = self.parser._dsc_reg['header']
+        header_type = "Type=i16 [X,Y,C] width=256 height=256"
+        matches = header_matcher.match(header_type).groupdict()
+        type_ = matches['type']
+        self.assertEqual(type_, "i16 [X,Y,C]")
+        width = matches['width']
+        self.assertEqual(width, "256")
+        height = matches['height']
+        self.assertEqual(height, "256")
+
+    def test_can_analyse_dsc_header(self):
+        header = [
+                "A0000001",
+                "[F0]",
+                "Type=i16 [X,Y,C] width=256 height=256",
+                "\"Acq mode\" (\"Acquisition mode\")",
+                "i32[1]",
+                "1"]
+        expected_dict = {
+                'name': "A0000001",
+                'type': "i16 [X,Y,C]",
+                'width': 256,
+                'height': 256}
+        expected = (expected_dict, header[3:])
+        self.assertEqual(expected, self.parser._analyse_dsc_header(header))
+
+    def test_can_parse_config(self):
+        config = ["\"Hw timer\" (\"Hw timer mode\"):",
+                  "i32[1]",
+                  "2"]
+        expected = dict(hw_timer_mode=2)
+        self.assertEqual(expected, self.parser._parse_config(config))
+        config_list = config.copy()
+        config_list.pop()
+        config_list.append("1 2 3 4")
+        expected = dict(hw_timer_mode=[1, 2, 3, 4])
+        self.assertEqual(expected, self.parser._parse_config(
+            config_list, expect_list=[("Hw timer", " ")]))
+        config.pop(0)
+        config_alternate = [('[sS]tart time \(string\)', "start time string")]
+        config.insert(0, "Start time (string)")
+        expected = dict(start_time_string=2)
+        self.assertEqual(expected, self.parser._parse_config(config, use_alternate=config_alternate))
+
+
+    def test_can_create_frame_from_dsc(self):
+        self.assertIsInstance(
+            self.parser._frame_from_dsc(self.dsc_data), frame.Frame)
+
+    def test_creates_correct_frame_from_dsc(self):
+        other = self.parser._frame_from_dsc(self.dsc_data)
+        self.assertEqual(self.frame.__dict__, other.__dict__)
+
+    def creates_correct_frame_from_pickle(self):
+        other = pk.loads(self.parser._pickle_from_dsc(self.dsc_data))
+        self.assertEqual(self.frame.__dict__, other.__dict__)

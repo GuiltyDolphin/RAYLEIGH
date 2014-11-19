47720bcf834404d4dc09b8245df1220bb6fc6f6c
Ben Moon <guiltydolphin@gmail.com>
2014-11-19 09:31:05 +0000

    Added plotter write to directory ability (heatmap)

--- analysis/plotter.py
+++ analysis/plotter.py
@@ -4,14 +4,16 @@
 from matplotlib import pyplot as plt
 
 import numpy as np
+import json
+import os
 
 
 class GraphPlotter():
-    def _generate_with_coordinates(self, xs, ys, zs, size=(256, 256)):
+    def _generate_with_coordinates(self, frame, size=(256, 256)):
         """Populate an empty numpy array of size with zs
         elements at xs and ys coordinates"""
         arr = np.zeros(size)
-        for x, y, z in zip(xs, ys, zs):
+        for x, y, z in frame:
             arr.itemset((x, y), z)
         return arr
 
@@ -24,3 +26,41 @@ class GraphPlotter():
         ax.set_ylim((0, 255))
         ax.set_xlim((0, 255))
         return fig, ax
+
+    def _gen_heatmap(self, data):
+        """Generate a heatmap figure
+
+        Parameters
+        ----------
+        data : (ndarray)
+             The data to be plotted on the heatmap
+
+        Returns
+        -------
+        fig : (Figure)
+            The figure object
+        ax  : (Axes)
+            The axes object
+        heatmap : (Todo: Unknown)
+            The actual heatmap object
+        """
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        heatmap = ax.pcolormesh(data, cmap=plt.cm.Reds)
+        ax.xaxis.set_label("X coordinate")
+        ax.yaxis.set_label("Y coordinate")
+        return fig, ax, heatmap
+
+    def _gen_heatmap_from_file(self, file_name):
+        with open(file_name) as f:
+            frame = json.load(f)
+        data = self._generate_with_coordinates(frame)
+        return self._gen_heatmap(data)
+
+    def _write_heatmap_from_file(self, input_file, output=None):
+        fig, _, _ = self._gen_heatmap_from_file(input_file)
+        print(os.listdir(os.path.dirname(input_file)))
+        os.mkdir(os.path.dirname(input_file) + "/plots")
+        fig.savefig(output or "{}/plots/{}.png".format(
+            os.path.dirname(input_file), os.path.basename(input_file)))
+        print(os.listdir(os.path.dirname(input_file) + "/plots"))
--- tests/test_plotter.py
+++ tests/test_plotter.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import tempfile
+import os
+import shutil
+import json
+
+#from matplotlib.testing.decorators import image_comparison
+import matplotlib
 
 from analysis import plotter
 
@@ -11,16 +18,28 @@ class TestFrameGraphing(unittest.TestCase):
     """Tests for graph output from the Graph Plotter"""
 
     def setUp(self):
-        self.xyz = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+        self.xyz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         self.xs = [1, 4, 7]
         self.ys = [2, 5, 8]
         self.zs = [3, 6, 9]
         self.plotter = plotter.GraphPlotter()
         self.fig, self.ax = self.plotter._generate_basic_figure()
-        self.heatmap_data = self.plotter._generate_with_coordinates(self.xs, self.ys, self.zs)
+        self.heatmap_data = self.plotter._generate_with_coordinates(self.xyz)
+
+        self.dir = tempfile.mkdtemp()
+        self.in_file_frame = tempfile.NamedTemporaryFile(delete=False, dir=self.dir)
+        with open(self.in_file_frame.name, 'w') as f:
+            f.write(json.dumps(self.xyz))
+
+        def get_new_file_name(fname):
+            base = os.path.basename(fname)
+            new_name = os.path.splitext(base)[0] + ".png"
+            return self.dir + "/plots/" + new_name
+        self.out_name = get_new_file_name(self.in_file_frame.name)
 
     def tearDown(self):
-        pass
+        os.remove(self.in_file_frame.name)
+        shutil.rmtree(self.dir)
 
     def test_inserts_elements_at_correct_indices(self):
         arr_vals = [self.heatmap_data[x, y] for (x, y) in zip(self.xs, self.ys)]
@@ -43,4 +62,32 @@ class TestFrameGraphing(unittest.TestCase):
         # and compare against the known points of the prevously
         # generated numpy array
         #self.assertEqual(self.heatmap_data,
+
+    def test_can_write_output_file_to_directory(self):
+        self.plotter._write_heatmap_from_file(self.in_file_frame.name)
+        expected_contents = [os.path.basename(self.out_name)]
+        actual = os.listdir(self.dir + "/plots")
+        self.assertCountEqual(expected_contents, actual)
+
+
+#@image_comparison(baseline_images=['heatmap_test_graph'])
+#def test_basic_figure_heatmap_correct_axes():
+#    #fig = self.plotter._generate_basic_figure()
+#    xs = [1, 4, 7]
+#    ys = [2, 5, 8]
+#    zs = [3, 6, 9]
+#    plotter_ = plotter.GraphPlotter()
+#    heatmap_data = plotter_._generate_with_coordinates(xs, ys, zs)
+#    fig, ax, heatmap = plotter_._gen_heatmap(heatmap_data)
+#    #fig.savefig('tests/heatmap_test_graph')
+#    #fig, self.ax = plotter._generate_basic_figure()
+#    def can_generate_data_from_frame_file(self):
+#        self.assertEqual(self.plotter._gen_heatmap_data_from_frame(
+   # def test_can_produce_heatmap_from_file(self):
+   #     fig, ax, _ = self.plotter._gen_heatmap_from_file(self.in_file_frame.name)
+   #     actual = ax.gca().get_lines()[0]
+   #     fig2, _, _ = self.plotter._gen_heatmap(self.heatmap_data)
+   #     #expected = self.plotter._gen_heatmap(self.heatmap_data)
+   #     expected = fig2.gca().get_lines()[0]
+   #     self.assertEqual(actual, expected)
 

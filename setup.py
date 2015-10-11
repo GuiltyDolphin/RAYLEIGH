c11c005c74ffa4134afa287f962dbfe85b880a64
Ben Moon <guiltydolphin@gmail.com>
2015-10-11 18:06:38 +0100

    Add package metadata

--- setup.py
+++ setup.py
@@ -2,9 +2,28 @@
 # -*- coding: utf-8 -*-
 # setup.py
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='RAYLEIGH',
+    version="0.1.0",
+    packages=find_packages(),
+    scripts=[
+        'analysis/frame_parser.py',
+        'analysis/plotter.py',
+        'analysis/dsc_parser.py'],
+
+    install_requires=[
+        'matplotlib>=1.4.2', 'nose>=1.3.4',
+        'numpy>=1.9.0', 'setuptools>=6.0.2'],
+
+    author="GuiltyDolphin",
+    author_email="GuiltyDolphin@gmail.com",
+    description="A basic plotter and converter for output files from"
+    + " TimePix chips.",
+    license="MIT",
+    keywords="plotter ray rayleigh json converter",
+    url="https://www.github.com/GuiltyDolphin/RAYLEIGH",
+
     test_suite='nose.collector'
     )
